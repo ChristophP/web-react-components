df1be80a338fe04bb17703a000e028690f3c5f1a
Lay Flags <code@layfla.gs>
2017-06-12 12:13:41 +0200

    dispatch events

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const parseAttribute = ({ name, value }) => {
-  // treat on... attribute values as code, like with e.g. native onclick
-  if (name.match(/on.+/)) {
-    return () => eval(value); // yes, that's evil ... like the native ones ;)
-  }
-
+const parseAttribute = (value) => {
   try {
     // try to parse attribute values as JSON, e.g.
     // - "1" -> 1
@@ -15,29 +10,43 @@ const parseAttribute = ({ name, value }) => {
     // - "[1, null, \"foo\"]" -> [1, null, 'foo']
     return JSON.parse(value);
   } catch (err) {
-    // convert attribute values like "foo,1,baz" to ['foo', '1', 'baz']
-    const values = value.split(',');
-    if (values.length > 1) return values;
-
     // or just return the raw value as string
     return value;
   }
 };
 
 // ATTENTION: all attribute names are lowercase in HTML, always!
-const convertAttributes = attributes =>
-  [].slice.call(attributes)
-    .reduce((obj, attr) =>
-      ({ ...obj, [attr.name]: parseAttribute(attr) }),
-      {}
-    );
+const convertAttributes = (node, attributeNames) =>
+  attributeNames.reduce((obj, name) => {
+    const value = node.getAttribute(name.toLowerCase());
+
+    return ({ ...obj, [name]: parseAttribute(value) });
+  }, {});
+
+const convertEvents = (node, eventNames) =>
+  eventNames.reduce((obj, name) => {
+    const domAttrName = name.toLowerCase();
+    const domAttrValue = node.getAttribute(domAttrName);
+    const domEventName = domAttrName.replace(/^on/, '');
+
+    return ({
+      ...obj,
+      [name](data) {
+        const domEvent = new Event(domEventName, { bubbles: true });
+        domEvent.data = data;
+        node.dispatchEvent(domEvent);
+
+        if (domAttrValue) eval(domAttrValue);
+      },
+    });
+  }, {});
 
 const convertChildren = innerHTML =>
   innerHTML.trim() !== ''
     ? <div dangerouslySetInnerHTML={{__html: innerHTML }} />
     : null;
 
-export function register(ReactComponent, tagName) {
+export function register(ReactComponent, tagName, { attributes, events } = {}) {
   class WebReactComponent extends HTMLElement {
     attachedCallback() {
       this._origInnerHTML = this.innerHTML;
@@ -53,10 +62,15 @@ export function register(ReactComponent, tagName) {
     }
 
     renderElement() {
+      const attrs = {
+        ...convertAttributes(this, attributes),
+        ...convertEvents(this, events),
+      };
+
       ReactDOM.render(
         React.createElement(
           ReactComponent,
-          convertAttributes(this.attributes),
+          attrs,
           // FIXME: orig children cannot change later by external (non React)
           // DOM manipulation
           convertChildren(this._origInnerHTML),
