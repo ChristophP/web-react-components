81fb288561aa9a827d19fe710830b2824c8485dd
ChristophP <deedop@hotmail.de>
2017-06-18 23:29:21 +0200

    add other module types

--- webpack.config.js
+++ webpack.config.js
@@ -24,10 +24,14 @@ module.exports = {
   externals: {
     react: {
       commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react',
       root: 'React',
     },
     'react-dom': {
       commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'react-dom',
       root: 'ReactDOM',
     }
   },
